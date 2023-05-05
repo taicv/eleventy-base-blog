d758d244e4678831b6322f29e21a1992a19a5c4a
taicv <chuvantai@gmail.com>
2023-05-06 02:04:12 +0700

    Create blogposts.js

---
+++ _data/blogposts.js
@@ -0,0 +1,83 @@
+const fetch = require("node-fetch");
+
+// function to get blogposts
+async function getAllBlogposts() {
+  // max number of records to fetch per query
+  const recordsPerQuery = 100;
+
+  // number of records to skip (start at 0)
+  let recordsToSkip = 0;
+
+  let makeNewQuery = true;
+
+  let blogposts = [];
+
+  // make queries until makeNewQuery is set to false
+  while (makeNewQuery) {
+    try {
+      // initiate fetch
+      const data = await fetch("https://strapi.grisfox.com/graphql", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          query: `{
+              articles {
+              id
+              title
+              content
+              published
+              author
+              slug
+            }
+          }`,
+        }),
+      });
+
+      // store the JSON response when promise resolves
+      const response = await data.json();
+
+      // handle CMS errors
+      if (response.errors) {
+        let errors = response.errors;
+        errors.map((error) => {
+          console.log(error.message);
+        });
+        throw new Error("Houston... We have a CMS problem");
+      }
+
+      // update blogpost array with the data from the JSON response
+      blogposts = blogposts.concat(response.data.articles);
+
+      // prepare for next query
+      recordsToSkip += recordsPerQuery;
+
+      // stop querying if we are getting back less than the records we fetch per query
+      if (response.data.articles.length < recordsPerQuery) {
+        makeNewQuery = false;
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  // format blogposts objects
+  const blogpostsFormatted = blogposts.map((item) => {
+    return {
+      id: item.id,
+      title: item.title,
+      slug: item.slug,
+      body: item.content,
+      author: item.author,
+      date: item.published     
+    };
+  });
+
+  // return formatted blogposts
+  return blogpostsFormatted;
+}
+
+// export for 11ty
+module.exports = getAllBlogposts;
